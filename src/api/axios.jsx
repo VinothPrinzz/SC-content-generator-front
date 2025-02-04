748f89781efe3e9ed48f3c5407504a8c3885a477
vinoth m <120578231+VinothPrinzz@users.noreply.github.com>
2025-02-04 23:18:44 +0530

    Update axios.jsx

--- src/api/axios.jsx
+++ src/api/axios.jsx
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000' // Replace with your backend URL/port
+    baseURL: 'https://sc-content-generator-back.onrender.com' // Replace with your backend URL/port
 });
 
 export default api;
