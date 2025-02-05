f792d53bf643fe6122e6df9f61cd8f0ef1f41c05
VinothPrinzz <VinothPrinzz@gmail.com>
2025-02-05 22:14:01 +0530

    redirects error updated

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
-    },
+  server: {
+    port: 3000
   },
-});
+  build: {
+    outDir: 'dist'
+  }
+})
